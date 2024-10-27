3d0a2d7725c9999c861c7f471ad71f1999b48b31
bryakotkin <n.bryakotkin@gmail.com>
2024-10-27 03:59:40 +0300

    Complete stash task

--- stash-file-project/main.cpp
+++ stash-file-project/main.cpp
@@ -11,19 +11,36 @@
 // https://gist.github.com/leommoore/f9e57ba2aa4bf197ebc5
 
 // Пример использования openssl
-// openssl enc -aes-128-ctr -pbkdf2 -in ./1.txt -out ./1_enc.txt -pass pass:1234 - enc
-// openssl enc -aes-128-ctr -pbkdf2 -d -in ./1_enc.txt -out ./3.txt -pass pass:1234 - decrypt\
+// openssl enc -aes-128-ctr -pbkdf2 -in ./1.txt -out ./1_enc.txt -pass pass:1234 - encrypt
+// openssl enc -aes-128-ctr -pbkdf2 -d -in ./1_enc.txt -out ./3.txt -pass pass:1234 - decrypt
 
 // https://wiki.openssl.org/index.php/EVP_Symmetric_Encryption_and_Decryption
 // https://github.com/mohabouz/CPP-AES-OpenSSL-Encrypt/blob/master/utils.cpp
 
-const int KEY_SIZE = 16;  // 128 бит = 16 байт
-const int IV_SIZE = 16;   // IV для режима CTR также 16 байт
+const int KEY_SIZE = 16;
+const int IV_SIZE = 16;
 const int ENCRYPTED_BYTES_SIZE = 8;
+
+unsigned char KEY[KEY_SIZE] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35 };
+unsigned char IV[IV_SIZE] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35 };
 
 enum Mode {
     ENCRYPT,
-    DECRYPT
+    DECRYPT,
+    INVALID_MODE
+};
+
+struct ModeHandler {
+public:
+    Mode map(const std::string& modeString) {
+        if (modeString == "encrypt") {
+            return ENCRYPT;
+        } else if (modeString == "decrypt") {
+            return DECRYPT;
+        } else {
+            return INVALID_MODE;
+        }
+    }
 };
 
 int encrypt(
@@ -88,24 +105,6 @@ int decrypt(
     return outputLen;
 }
 
-unsigned char* encryptHeader(unsigned char* headerText, unsigned char* key, unsigned char* iv) {
-    unsigned int size = std::strlen((char *) headerText);
-    unsigned char* cipher = (unsigned char *) malloc(size);
-
-    encrypt(headerText, size, key, iv, cipher);
-
-    return cipher;
-}
-
-unsigned char* decryptHeader(unsigned char* ecryptedHeaderText, unsigned char* key, unsigned char* iv) {
-    unsigned int size = std::strlen((char *) ecryptedHeaderText);
-    unsigned char* decryptedText = (unsigned char *) malloc(size);
-
-    decrypt(ecryptedHeaderText, size, key, iv, decryptedText);
-
-    return decryptedText;
-}
-
 bool validatePath(const std::string& userPath, std::error_code& error_code) {
     const std::filesystem::path path(userPath);
     if (std::filesystem::is_directory(path, error_code)) {
@@ -124,7 +123,7 @@ bool validatePath(const std::string& userPath, std::error_code& error_code) {
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        std::cout << "Use: " << argv[0] << " <path to file>" << std::endl;
+        std::cout << "Use: " << argv[0] << "<mode> <path to file>" << std::endl;
         return -1;
     }
     std::error_code error_code;
@@ -132,10 +131,6 @@ int main(int argc, char *argv[]) {
         std::cerr << "Validation failure with error: " << error_code.message() << std::endl;
         return -1;
     }
-    
-    // oh no cringe
-    unsigned char key[KEY_SIZE] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35 };
-    unsigned char iv[IV_SIZE] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35 };
 
     std::fstream file(argv[2], std::ios::in | std::ios::out | std::ios::binary); 
     if (!file) { 
@@ -143,33 +138,37 @@ int main(int argc, char *argv[]) {
         return -1; 
     }
 
-    std::map <std::string, Mode> mapping;
-    mapping["decrypt"] = DECRYPT;
-    mapping["encrypt"] = ENCRYPT;
+    ModeHandler modeHandler;
+    Mode mode = modeHandler.map(argv[1]);
+    
+    const int size = ENCRYPTED_BYTES_SIZE;
+    char* headerText = (char *) malloc(size + 1);
+    unsigned char* result = (unsigned char *) malloc(size + 1);
 
-    unsigned char* encrypted = nullptr;
-    unsigned char* decrypted = nullptr;
-    char* headerTextToEncrypt = (char *) malloc(ENCRYPTED_BYTES_SIZE + 1);
-    char* headerTextToDecrypt = (char *) malloc(ENCRYPTED_BYTES_SIZE + 1);
-    switch (mapping[argv[1]]) {
+    switch (mode) {
         case ENCRYPT:
-            file.read(headerTextToEncrypt, ENCRYPTED_BYTES_SIZE);
-            encrypted = encryptHeader((unsigned char *) headerTextToEncrypt, key, iv);
+            file.read(headerText, size);
+            encrypt((unsigned char*) headerText, size, KEY, IV, result);
             file.seekp(0);
-            file.write((char *) encrypted, ENCRYPTED_BYTES_SIZE);
-            file.close();
+            file.write((char *) result, size);
             break;
         case DECRYPT:
-            file.read(headerTextToDecrypt, ENCRYPTED_BYTES_SIZE);
-            decrypted = decryptHeader((unsigned char *) headerTextToDecrypt, key, iv);
+            file.read(headerText, size);
+            decrypt((unsigned char *) headerText, size, KEY, IV, result);
             file.seekp(0);
-            file.write((char *) decrypted, ENCRYPTED_BYTES_SIZE);
-            file.close();
+            file.write((char *) result, size);
             break;
         default:
+            std::cerr << "Invalid mode!" << std::endl;
+            free(headerText);
+            free(result);
             file.close();
             return -1;
     }
+
+    free(headerText);
+    free(result);
+    file.close();
     
     return 0;
 }
